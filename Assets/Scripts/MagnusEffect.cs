d95d592364396dad3e56af5d6ecc73f02a1677e4
THANAKRIT16207 <thanakrit.sukk@bumail.net>
2026-03-28 04:50:11 +0700

    Add script to ball

--- Assets/Scripts/MagnusEffect.cs
+++ Assets/Scripts/MagnusEffect.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class MagnusEffect : MonoBehaviour
 {
     public float kickForce = 1.0f;
-    public float spinAmount = 1.0f; // ปรับทิศทางหลังเตะไปแล้ว ซ้าย/ขวา
+    public float spinAmount = 1.0f;
     public float magnusStrength = 0.5f;
     private Rigidbody _rb;
     private bool _isShoot = false;
@@ -16,9 +16,7 @@ public class MagnusEffect : MonoBehaviour
     {
         if (Keyboard.current.spaceKey.wasPressedThisFrame && !_isShoot)
         {
-            // ปรับทิศทางตอนเตะ x, y, z
             _rb.AddForce(-1, 4f, kickForce, ForceMode.Impulse);
-            // ให้ลูกบอลหมุนเวลาเคลื่อนที่
             _rb.AddRelativeTorque(Vector3.up * spinAmount);
             _isShoot = true;
         }
@@ -28,7 +26,6 @@ public class MagnusEffect : MonoBehaviour
         if (!_isShoot) return;
         Vector3 velocity = _rb.linearVelocity;
         Vector3 spin = _rb.angularVelocity;
-        // Cross Product หาทิศทางใหม่ให้บอลเคลื่อนที่ไป
         Vector3 magnusForce = Vector3.Cross(spin, velocity);
         magnusForce *= magnusStrength;
         _rb.AddForce(magnusForce);
